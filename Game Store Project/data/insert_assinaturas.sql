4ebda7e343b9d23de14e1ef919d1d85bd36ee7f2
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-05-20 14:18:16 -0300

    Add files via upload

---
+++ Game Store Project/data/insert_assinaturas.sql
@@ -0,0 +1,46 @@
+INSERT INTO product (product_name, category_id, quantity_per_unit, unit_price, units_in_stock, discontinued) VALUES
+-- PlayStation Plus
+('PlayStation Plus Essential - 1 Mês', 5, 1, 34.90, 500, false),
+('PlayStation Plus Essential - 3 Meses', 5, 1, 89.90, 300, false),
+('PlayStation Plus Essential - 12 Meses', 5, 1, 199.90, 200, false),
+('PlayStation Plus Extra - 12 Meses', 5, 1, 399.90, 150, false),
+('PlayStation Plus Premium - 12 Meses', 5, 1, 499.90, 100, false),
+
+-- Xbox Game Pass
+('Xbox Game Pass Core - 1 Mês', 5, 1, 29.90, 600, false),
+('Xbox Game Pass Core - 3 Meses', 5, 1, 74.90, 400, false),
+('Xbox Game Pass Core - 12 Meses', 5, 1, 179.90, 250, false),
+('Xbox Game Pass Ultimate - 1 Mês', 5, 1, 44.90, 450, false),
+('Xbox Game Pass Ultimate - 3 Meses', 5, 1, 119.90, 300, false),
+('Xbox Game Pass Ultimate - 12 Meses', 5, 1, 359.90, 180, false),
+
+-- Nintendo Switch Online
+('Nintendo Switch Online - 1 Mês', 5, 1, 19.90, 700, false),
+('Nintendo Switch Online - 3 Meses', 5, 1, 49.90, 500, false),
+('Nintendo Switch Online - 12 Meses', 5, 1, 119.90, 350, false),
+('Nintendo Switch Online + Expansion Pack - 12 Meses', 5, 1, 229.90, 200, false),
+
+-- Serviços de Nuvem
+('GeForce NOW Priority - 1 Mês', 5, 1, 49.90, 300, false),
+('GeForce NOW Priority - 6 Meses', 5, 1, 249.90, 150, false),
+('Boosteroid - 1 Mês', 5, 1, 39.90, 250, false),
+('Boosteroid - 12 Meses', 5, 1, 359.90, 120, false),
+
+-- Outros Serviços
+('EA Play - 1 Mês', 5, 1, 24.90, 400, false),
+('EA Play - 12 Meses', 5, 1, 129.90, 200, false),
+('Ubisoft+ - 1 Mês', 5, 1, 79.90, 180, false),
+('Xbox Live Gold - 12 Meses (Legado)', 5, 1, 199.90, 80, true),
+
+-- Gift Cards
+('Gift Card PSN - R$ 50', 5, 1, 50.00, 300, false),
+('Gift Card PSN - R$ 100', 5, 1, 100.00, 250, false),
+('Gift Card Xbox - R$ 50', 5, 1, 50.00, 280, false),
+('Gift Card Xbox - R$ 100', 5, 1, 100.00, 230, false),
+('Gift Card Nintendo - R$ 100', 5, 1, 100.00, 190, false),
+
+-- Pacotes Promocionais
+('Pacote Gamer Essential: PS Plus + Gift Card R$ 50', 5, 1, 229.90, 100, false),
+('Pacote Ultimate: Game Pass Ultimate + EA Play', 5, 1, 399.90, 80, false),
+('Pacote Família: Nintendo Online + Expansion 12 Meses', 5, 1, 399.90, 60, false);
+

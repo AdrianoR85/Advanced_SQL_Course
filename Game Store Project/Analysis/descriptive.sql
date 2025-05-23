a84797ea60f20c486f0896366b341a39bc013de0
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-05-23 19:41:33 -0300

    Update descriptive.sql

--- Game Store Project/Analysis/descriptive.sql
+++ Game Store Project/Analysis/descriptive.sql
@@ -4,7 +4,7 @@ SELECT
 	SUM(total_price)
 FROM purchase
 GROUP BY sale_year;
-
+-----------------------------------------------------------------------------------------------------
 -- → Which products are the best-selling in quantity annually?
 SELECT 
 	date_part('year', p.purchase_date) AS sale_year,
@@ -15,7 +15,7 @@ JOIN purchase_item pi ON pi.purchase_id = p.purchase_id
 JOIN product pr ON pi.product_id = pr.product_id
 GROUP BY sale_year, product
 ORDER BY sale_year, quantity_total DESC;
-
+-----------------------------------------------------------------------------------------------------
 -- → Which products generate the highest revenue annually?
 WITH ranked_product AS (
 	SELECT 
@@ -35,3 +35,27 @@ SELECT
 FROM ranked_product
 WHERE rank = 1
 ORDER BY sale_year;
+
+-----------------------------------------------------------------------------------------------------
+-- →  What is the average number of sales per month?
+SELECT
+	sales.sale_year,
+	sales.total_sales,
+	ROUND(sales.total_sales::NUMERIC / monthly.months_with_sales,2) AS avg_monthly_sales
+FROM (	
+	SELECT 
+		DATE_PART('year', purchase_date) AS sale_year,
+		COUNT(purchase_id) AS total_sales
+	FROM purchase
+	GROUP BY sale_year
+) AS sales
+JOIN (
+	-- Count the number of days with sales in each year 
+	SELECT 
+		DATE_PART('year', purchase_date) AS sale_year,
+		COUNT(DISTINCT DATE_PART('month', purchase_date)) AS months_with_sales
+	FROM purchase
+	GROUP BY sale_year
+) AS monthly
+ON sales.sale_year = monthly.sale_year
+ORDER BY sales.sale_year;

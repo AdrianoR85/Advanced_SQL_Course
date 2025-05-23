8175e068633f71066b9de4d7d58ad0792c019a06
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-05-23 18:53:52 -0300

    Update descriptive.sql

--- Game Store Project/Analysis/descriptive.sql
+++ Game Store Project/Analysis/descriptive.sql
@@ -1,48 +1,37 @@
--- Fase 1 — SQL Descritivo (Exploratórios)
--- Use SELECT, GROUP BY, ORDER BY, JOIN
-
--- 1. Produtos mais vendidos
-SELECT
-	p.product_name,
-	SUM(pi.quantity) AS quantity_sale,
-	RANK() OVER(ORDER BY SUM(pi.quantity) DESC) AS rank_quantity,
-	SUM(pi.price) AS revenue_sale,
-	RANK() OVER(ORDER BY SUM(pi.price) DESC) AS rank_revenue
-FROM product p
-JOIN purchase_item pi
-ON p.product_id = pi.product_id
-GROUP BY p.product_name
-ORDER BY quantity_sale DESC
-LIMIT 10;
-
--- 2. Receita total por mês
-SELECT
-	date_part('year',purchase_date) AS yearly_revenue,
-	date_part('month',purchase_date)AS monthly_revenue,
-	SUM(total_price) AS total_sale_by_month
-FROM purchase
-GROUP BY 
-	yearly_revenue,
-	monthly_revenue
-ORDER BY 
-	monthly_revenue,
-	yearly_revenue;
-
--- 3. Média de vendas por região
-SELECT
-	ship_region,
-	ROUND(AVG(total_price),2) AS average_total_sale
-FROM purchase
-GROUP BY ship_region;
-
--- 4. Total vendido por categoria
+-- → What is the annual revenue?
 SELECT 
-	c.name,
-	SUM(pi.quantity) AS quantity_sale,
-	SUM(pi.price) AS revenue_sale,
-	ROUND(SUM(pi.price) * 100.0 / SUM(SUM(pi.price)) OVER(),1) AS percent_revenue
-FROM category c
-JOIN product p ON c.category_id = p.category_id
-JOIN purchase_item pi ON p.product_id = pi.product_id
-GROUP BY c.name
-ORDER BY quantity_sale DESC;
+	date_part('year', purchase_date) AS sale_year,
+	SUM(total_price)
+FROM purchase
+GROUP BY sale_year;
+
+-- → Which products are the best-selling in quantity annually?
+SELECT 
+	date_part('year', p.purchase_date) AS sale_year,
+	pr.product_name AS product,
+	SUM(pi.quantity) AS quantity_total
+FROM purchase p
+JOIN purchase_item pi ON pi.purchase_id = p.purchase_id
+JOIN product pr ON pi.product_id = pr.product_id
+GROUP BY sale_year, product
+ORDER BY sale_year, quantity_total DESC;
+
+-- → Which products generate the highest revenue annually?
+WITH ranked_product AS (
+	SELECT 
+	date_part('year', p.purchase_date) AS sale_year,
+	pr.product_name AS product,
+	SUM(pi.price) AS revenue_anually,
+	ROW_NUMBER() OVER (PARTITION BY date_part('year', p.purchase_date) ORDER BY SUM(pi.price) DESC) AS rank 
+	FROM purchase p
+	JOIN purchase_item pi ON pi.purchase_id = p.purchase_id
+	JOIN product pr ON pi.product_id = pr.product_id
+	GROUP BY sale_year, product
+)
+SELECT 
+	sale_year,
+	product,
+	revenue_anually
+FROM ranked_product
+WHERE rank = 1
+ORDER BY sale_year;

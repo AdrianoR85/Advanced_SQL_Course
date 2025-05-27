e1ea80411028dbe96a5575b4be20cc356f457592
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-05-27 11:27:34 -0300

    Update Game_Store_Logistics.sql

--- Analysis/Descriptive/Game_Store_Logistics.sql
+++ Analysis/Descriptive/Game_Store_Logistics.sql
@@ -86,13 +86,30 @@ ORDER BY year, total DESC;
 
 --------------------------------------------------------------------------------------
 -- -> ABC Classification by product quantity in stock
+
+/*
+ What This Does
+- product_name: Name of the product.
+- units_in_stock: Quantity of that product in stock.
+- pct_by_product:	What % of total stock this product represents.
+- pct_accumulated: Running cumulative % from top product (by quantity) to bottom.
+- total_in_stock: Total units of all products in stock.
+- class_abc: 
+	A: Top ~80% of value → high impact products.
+	B: Next ~10% of value.
+	C: Remaining ~10%.
+*/
+
 WITH ranked_product_in_stock AS (
 	SELECT 
 		product_name,
 		units_in_stock,
 		ROUND(units_in_stock * 100.0 / SUM(units_in_stock) OVER(), 2) AS pct_by_product,
 		ROUND(
-			SUM(units_in_stock) OVER(ORDER BY units_in_stock DESC) * 100.0 /
+			SUM(units_in_stock) OVER(
+				ORDER BY units_in_stock DESC 
+				ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW 
+			) * 100.0 /
 			SUM(units_in_stock) OVER(), 2) AS pct_accumulated,
 		SUM(units_in_stock) OVER() AS total_in_stock
 	FROM product
@@ -105,3 +122,42 @@ SELECT
 		ELSE 'C'
 	END AS class_abc
 FROM ranked_product_in_stock
+
+--------------------------------------------------------------------------------------
+-- -> ABC Classification by product value in stock
+/* What This Does
+- product_value: Current value of each product in stock.
+- pct_by_product: What % of total value that product represents.
+- pct_accumulated: Cumulative % for ranking by value.
+- class_abc:
+	A: Top ~80% of value → high impact products.
+	B: Next ~10% of value.
+	C: Remaining ~10%.
+*/
+WITH ranked_product_in_stock AS (
+	SELECT 
+		product_name,
+		(units_in_stock * unit_price) AS product_value,
+		
+		-- % of each product value over total
+		ROUND((units_in_stock * unit_price) * 100.0 / SUM(units_in_stock * unit_price) OVER(), 2) AS pct_by_product,
+
+		-- Cumulative % (correct with ROWS clause)
+		ROUND(
+			SUM(units_in_stock * unit_price) OVER(
+				ORDER BY (units_in_stock * unit_price) DESC
+				ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW
+				) * 100.0 /
+			SUM(units_in_stock * unit_price) OVER(), 2) AS pct_accumulated,
+			
+		SUM(units_in_stock * unit_price) OVER() AS total_in_stock
+	FROM product
+)
+SELECT 
+	*,
+	CASE 
+		WHEN pct_accumulated <= 80 THEN 'A'
+		WHEN pct_accumulated <= 90 THEN 'B'
+		ELSE 'C'
+	END AS class_abc
+FROM ranked_product_in_stock;

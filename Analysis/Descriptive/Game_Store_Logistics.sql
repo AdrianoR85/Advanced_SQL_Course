af5162c700efb03808711732bd8c8472db8bcee7
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-05-27 10:29:23 -0300

    Update Game_Store_Logistics.sql

--- Analysis/Descriptive/Game_Store_Logistics.sql
+++ Analysis/Descriptive/Game_Store_Logistics.sql
@@ -17,6 +17,8 @@ WHERE shipped_date IS NOT NULL AND DATE_PART('year', purchase_date) = 2022
 GROUP BY 1, 2
 ORDER BY 1, 2;
 
+--------------------------------------------------------------------------------------
+
 -- -> ABC classification
 WITH product_sales AS (
     SELECT 
@@ -53,3 +55,32 @@ SELECT
     END AS class_abc
 FROM ranked_product
 ORDER BY total_quantity DESC;
+
+--------------------------------------------------------------------------------------
+
+-- What are the 5 products with the most sales?
+WITH ranked_sale AS (
+	SELECT
+		EXTRACT('year' FROM pu.purchase_date) AS year, 
+		p.product_name AS product,
+		SUM(pi.quantity) AS total,
+		ROW_NUMBER() OVER(
+			PARTITION BY EXTRACT('year' FROM pu.purchase_date)
+			ORDER BY SUM(pi.quantity) DESC
+		) AS sale_rank
+	FROM product p
+	JOIN purchase_item pi ON p.product_id = pi.product_id
+	JOIN purchase pu ON pi.purchase_id = pu.purchase_id
+	GROUP BY year, p.product_name
+)
+SELECT 
+	year,
+	product,
+	total,
+	sale_rank
+FROM ranked_sale
+WHERE sale_rank <= 5 and year = '2024'
+ORDER BY year, total DESC;
+
+--------------------------------------------------------------------------------------
+

82db9abdd6c723328f5377599eda9ed64e70077f
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-06-06 14:53:00 -0300

    Update subqueries.sql

--- Subqueries/subqueries.sql
+++ Subqueries/subqueries.sql
@@ -1,22 +1,12 @@
--- This query shows the customers who spent more than the average.
--- It joins the Customer and Purchase tables.
--- For each customer, it calculates the total money they spent (SUM of total_price).
--- It groups the results by customer.
--- Then, it uses a subquery in the HAVING clause.
--- The subquery calculates the average purchase value (AVG of total_price).
--- The HAVING clause checks if the customer's total is greater than that average.
+-- Most expensive product in the store.
+-- Using the subquery in WHERE clause.
 SELECT 
-    c.customer_id,
-    c.first_name,
-    c.last_name,
-    SUM(p.total_price) AS total_spent
+    product_id,
+    product_name,
+    unit_price
 FROM 
-    Customer c
-JOIN 
-    Purchase p ON c.customer_id = p.customer_id
-GROUP BY 
-    c.customer_id, c.first_name, c.last_name
-HAVING 
-    SUM(p.total_price) > (
-        SELECT AVG(total_price) FROM Purchase
-    );
+    Product
+WHERE 
+    unit_price = (
+        SELECT MAX(unit_price) FROM Product
+ );

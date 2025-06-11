5a6d8516210e8a402dcfd07741710e7e702b92be
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-06-11 11:11:23 -0300

    Update function.sql

--- Functions_and_Procedures/function.sql
+++ Functions_and_Procedures/function.sql
@@ -12,3 +12,27 @@ END
 $$ LANGUAGE plpgsql;
 
 select get_customer_purchase(10);
+
+
+-------------------------------------------------------------------------------------
+-- Get all puchase of customer 
+CREATE OR REPLACE FUNCTION get_customer_purchase(customer_id INT)
+RETURNS TABLE (
+    purchase_date TIMESTAMP,
+    total_price NUMERIC(10,2),
+    ship_city VARCHAR,
+    ship_state CHAR(2)
+) AS $$
+BEGIN
+    RETURN QUERY
+    SELECT 
+        p.purchase_date,
+        p.total_price,
+        p.ship_city,
+        p.ship_state
+    FROM purchase p
+    WHERE p.customer_id = get_customer_purchase.customer_id;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM get_customer_purchase(123);

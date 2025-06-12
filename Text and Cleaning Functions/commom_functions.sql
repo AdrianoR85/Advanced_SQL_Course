70b97a3b3f8a590e516720dda83548e047b436bc
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-06-12 16:44:55 -0300

    Update commom_functions.sql

--- Text and Cleaning Functions/commom_functions.sql
+++ Text and Cleaning Functions/commom_functions.sql
@@ -17,19 +17,6 @@ SELECT SPLIT_PART(product_name, ' ', 2 ) FROM product; -- Xbox One -> One
 
 SELECT * FROM product;
 SELECT * FROM customer;
+ 
 
----------------------------------------------------------------------------------------------------------------------------------------------- 
-
--- Function: format_camelcase_name_from_email
--- Description: Extracts and formats customer names written in CamelCase from the email address (before '@') by inserting spaces between name parts.
-
-SELECT 
-	SUBSTRING( email, 0, POSITION('@' IN email) ) AS names
-FROM customer
-SELECT 
-	TRIM( 
-		REGEXP_REPLACE (
-			SUBSTRING( email, 0, POSITION('@' IN email)), '([A-Z])', ' \1', 'g'
-		) 
-	) AS spaced_name
-FROM customer; 
+ 

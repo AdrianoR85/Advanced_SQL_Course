738b5c6189a1f851d309706bbdfa4bca63b5bdb1
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-06-29 13:05:05 -0300

    Update dim_employee.sql

--- Game Store Project/Gold/dim_employee.sql
+++ Game Store Project/Gold/dim_employee.sql
@@ -1,12 +1,12 @@
 CREATE OR REPLACE VIEW gold.dim_employee AS
 SELECT
 	e.employee_id,
-	CONCAT(e.first_name, ' ', e.last_name)::VARCHAR(50) AS employee_name,
-	DATE_PART('YEAR', AGE(e.birth_date))::INT AS age,
+	CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
+	DATE_PART('YEAR', AGE(e.birth_date)),
 	
 	CASE
-		WHEN e.reports_to IS NULL THEN 'Manager'::VARCHAR(50)
-		ELSE CONCAT(m.first_name, ' ', m.last_name)::VARCHAR(50)
+		WHEN e.reports_to IS NULL THEN 'Manager'
+		ELSE CONCAT(m.first_name, ' ', m.last_name)
 	END AS manager_name,
 
 	CASE
@@ -15,10 +15,12 @@ SELECT
 		WHEN y = 0 AND m > 0 THEN CONCAT(m, ' months')
 		ELSE 'Less than a month'
 	END company_time,
+	e.hire_date,
+	e.fired_date,
 
 	CASE
-		WHEN e.fired_date IS NULL THEN 'Active'::VARCHAR(12)
-		ELSE 'Terminated'::VARCHAR(12)
+		WHEN e.fired_date IS NULL THEN 'Active'
+		ELSE 'Terminated'
 	END employment_status
 	
 FROM (

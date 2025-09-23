4416df58a0a6f461c809ebc5ca8970fb869ea04c
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-09-23 13:35:57 -0300

    Update constraints.sql

--- Mercado_y_DB/constraints.sql
+++ Mercado_y_DB/constraints.sql
@@ -7,3 +7,15 @@ ALTER TABLE produto.tb_produto ADD CONSTRAINT chk_tb_produto_preco_venda CHECK(p
 -- CONSTRAINT UNIQUE
 ALTER TABLE produto.tb_fabricante ADD CONSTRAINT uc_tb_fabricante_email UNIQUE(email);
 ALTER TABLE produto.tb_categoria DROP COLUMN email;
+
+-- CONSTRAINT PRIMARY KEY
+ALTER TABLE produto.tb_produto ADD CONSTRAINT pk_id_produto PRIMARY KEY (id_produto);
+ALTER TABLE produto.tb_fabricante ADD CONSTRAINT pk_id_fabricante PRIMARY KEY (id_fabricante);
+ALTER TABLE produto.tb_categoria ADD CONSTRAINT pk_id_categoria PRIMARY KEY (id_categoria);
+
+-- CONSTRAINT FOREIGN KEY
+ALTER TABLE produto.tb_produto ADD CONSTRAINT fk_id_fabricante 
+FOREIGN KEY (fk_id_fabricante) REFERENCES produto.tb_fabricante (id_fabricante);
+
+ALTER TABLE produto.tb_produto ADD CONSTRAINT fk_id_categoria 
+FOREIGN KEY (fk_id_categoria) REFERENCES produto.tb_categoria(id_categoria);

27e49a3e6ded64b2382d0ba0b9b37eeef4f11826
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-09-29 10:13:45 -0300

    Update triggers.sql

--- Mercado_y_DB/triggers.sql
+++ Mercado_y_DB/triggers.sql
@@ -31,3 +31,37 @@ AFTER INSERT ON produto.tb_produto
 FOR EACH ROW
 EXECUTE PROCEDURE produto.func_log_produto()
 
+---
+
+CREATE OR REPLACE FUNCTION produto.fun_log_produto_update()
+RETURNS TRIGGER AS
+$$
+BEGIN
+	INSERT INTO auditoria.tb_produto_log(
+		id_produto,
+		id_categoria_new, id_categoria_old, 
+		id_fabricante_new, id_fabricante_old, 
+		produto_new, produto_old, 
+		preco_compra_new, preco_compra_old, 
+		preco_venda_new, preco_venda_old, 
+		evento)
+	VALUES(
+		OLD.id_produto,
+		NEW.fk_id_categoria, OLD.fk_id_categoria, 
+		NEW.fk_id_fabricante, OLD.fk_id_fabricante, 
+		NEW.produto, OLD.produto, 
+		NEW.preco_compra, OLD.preco_compra, 
+		NEW.preco_venda, OLD.preco_venda, 
+		'update'
+	);
+
+	RETURN NEW;
+
+END
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER tr_before_update_produto
+BEFORE UPDATE ON  produto.tb_produto
+FOR EACH ROW
+EXECUTE PROCEDURE produto.fun_log_produto_update();

2ac7dd4fa6226e4148c83341704a16515a2c6cc1
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-09-30 12:00:48 -0300

    Update inserts.sql

--- Mercado_y_DB/inserts.sql
+++ Mercado_y_DB/inserts.sql
@@ -1,14 +1,57 @@
-insert into produto.tb_categoria(categoria)values('Bebiddas');
-select * from produto.tb_categoria;
+-- Insert Categorias
+INSERT INTO produto.tb_categoria(categoria) VALUES ('Bebidas');
+INSERT INTO produto.tb_categoria(categoria) VALUES ('Mercearia');
+INSERT INTO produto.tb_categoria(categoria) VALUES ('Limpeza');
+INSERT INTO produto.tb_categoria(categoria) VALUES ('Higiene e Perfumaria');
 
-insert into produto.tb_fabricante(fabricante, telefone, contato)
-values('Coca-Cola', '11 5555-4444', 'Maria');
-select * from produto.tb_fabricante;
+-- Insert Produtos
+INSERT INTO produto.tb_produto(
+	fk_id_categoria, fk_id_fabricante, produto, 
+	unidade_medida, preco_compra, preco_venda)
+VALUES (1, 1, 'Refrigerante Coca-Cola 2L', 'UN', 3.35, 6.25);
 
-insert into produto.tb_produto
-	(fk_id_categoria, fk_id_fabricante, produto, unidade_medida,
-	preco_compra, preco_venda)
-	values
-	(3, 3, 'Refrigerante Coca-Cola 2,5L', 'UN',
-	 4.10, 6.85);
-select * from produto.tb_produto;
+INSERT INTO produto.tb_produto(
+	fk_id_categoria, fk_id_fabricante, produto,
+	unidade_medida, preco_compra, preco_venda )
+VALUES (2,2, 'Café tradicional 500g', 'UN', 5.12, 7.48);
+
+INSERT INTO produto.tb_produto(
+	fk_id_categoria, fk_id_fabricante, produto,
+	unidade_medida, preco_compra, preco_venda )
+VALUES (3,3, 'Lã de aço', 'UN', 1.00, 1.75);
+
+INSERT INTO produto.tb_produto(
+	fk_id_categoria, fk_id_fabricante, produto,
+	unidade_medida, preco_compra, preco_venda )
+VALUES (4, 4, 'Creme dental 90g', 'UN', 2.05, 2.65);
+
+
+-- Insert Fabricantes
+INSERT INTO produto.tb_fabricante(fabricante, telefone, contato)
+VALUES('Coca-cola', '11 5555-55555', 'Maria');
+
+INSERT INTO produto.tb_fabricante(fabricante, telefone, contato)
+VALUES('3 Corações', '11 4444-4444', 'João');
+
+INSERT INTO produto.tb_fabricante(fabricante, telefone, contato)
+VALUES('Bombril', '11 7777-7744', 'Alana');
+
+INSERT INTO produto.tb_fabricante(fabricante, telefone, contato)
+VALUES('Colgate', '11 2222-4411', 'Mauro');
+
+
+-- Insert Movimentações
+INSERT INTO estoque.tb_movimentacao (
+	fk_id_produto, fk_id_estoque_produto, qtde_movimento, tipo_movimento
+) VALUES (
+	3,
+	(SELECT id_estoque_produto FROM estoque.tb_estoque_produto WHERE fk_id_produto = 3),
+	25,
+	'entrada'
+);
+
+ALTER TABLE estoque.tb_movimentacao
+ADD CONSTRAINT fk_tb_movimentacao_estoque
+FOREIGN KEY (fk_id_estoque_produto) REFERENCES estoque.tb_estoque_produto(id_estoque_produto);
+
+

6e4c25cfea66e8ff0bf210370e0664778157b669
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-10-02 15:37:58 -0300

    Create selects.sql

---
+++ Mercado_y_DB/selects.sql
@@ -0,0 +1,10 @@
+SELECT * FROM produto.tb_categoria;
+SELECT * FROM produto.tb_fabricante;
+SELECT * FROM produto.tb_produto;
+
+SELECT * FROM auditoria.tb_produto_log;
+
+SELECT * FROM estoque.tb_estoque_produto;
+SELECT * FROM estoque.tb_movimentacao;
+
+SELECT * FROM venda.tb_venda;

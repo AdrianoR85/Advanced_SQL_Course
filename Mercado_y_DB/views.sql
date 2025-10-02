2dac8a0f0561d72b7ad654ac78eda819472e7a47
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-10-02 16:00:36 -0300

    Create views.sql

---
+++ Mercado_y_DB/views.sql
@@ -0,0 +1,28 @@
+/* To Create a View
+	CREATE VIEW <schema>.<view_name> AS
+  Select <cols> FROM <schema><table_name>
+*/
+
+/* To Drop a View
+	DROP VIEW <schema>.<view_name>
+*/
+
+/* To Rename a View
+	ALTER VIEW <schema>.<view_name> RENAME TO <new_view_name>
+*/
+
+-- Stock Position
+CREATE VIEW estoque.vw_posicao_estoque AS
+SELECT 
+	prod.id_produto,
+	prod.produto,
+	cat.categoria,
+	fab.fabricante,
+	prod.unidade_medida,
+	prod.preco_compra,
+	prod.preco_venda,
+	estp.qtde_estoque
+FROM estoque.tb_estoque_produto AS estp
+LEFT JOIN produto.tb_produto prod ON estp.fk_id_produto = prod.id_produto
+LEFT JOIN produto.tb_categoria cat ON prod.fk_id_categoria = cat.id_categoria
+LEFT JOIN produto.tb_fabricante fab ON prod.fk_id_fabricante = fab.id_fabricante;

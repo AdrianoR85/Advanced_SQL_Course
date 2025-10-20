41ff311f30880a8f3497c72f7b20189a2113a437
ADRIANO ROSA <73034540+AdrianoR85@users.noreply.github.com>
2025-10-20 20:45:35 -0300

    Update monitoring.sql

--- DBA Commands/monitoring.sql
+++ DBA Commands/monitoring.sql
@@ -4,6 +4,13 @@
 SELECT * FROM pg_catalog.pg_stat_activity;
 -- Concelando a execução de uma query.
 SELECT pg_cancel_backend(20339);
-SELECT pg_cancel_backend(pid) FROM pg_catalog.pg_stat_activity WHERE usaname = 'site' -- cancela tudo daquele usuário
+
+SELECT pg_cancel_backend(pid) 
+FROM pg_catalog.pg_stat_activity 
+WHERE usaname = 'site' -- cancela tudo daquele usuário
 -- Finalizar a sessão
 SELECT pg_terminate_backend(20339);
+-- Finalizando um sessão específica
+SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE datname = 'db_1';
